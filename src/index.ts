eaf4aca59ea7f38b92affba640cce03b936e56dd
sdegutis <sbdegutis@gmail.com>
2020-05-21 14:01:09 -0500

    Hide console on start

--- src/index.ts
+++ src/index.ts
@@ -130,7 +130,7 @@ const createWindow = () => {
   mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
 
   mainWindow.on('close', (event) => {
     if (!budgets[mainWindow.id].confirmQuit()) {
